cb0e9fe044e5968acb454a80e25e1fa7bfbe9f18
Olivier Girardot <ogmusik@gmail.com>
2020-09-28 11:35:58 +0200

    managed to make pdfmake on the server side, but with old versions of the build. asked in forum why not working with latest builds

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const router = express.Router();
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/')));
+
+
+// This line enables the programm to read content from the html body
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
     res.send("OMG it's working!");
@@ -18,6 +23,13 @@ app.get('/', (req, res) => {
 app.get('/folder', (req, res) => {
     res.sendFile('test2.html');
 });
+
+///////////////Create PDF////////////////////
+const pdfRoute = require('./form.js')
+app.use('/pdfMake', pdfRoute)
+
+/////////////////////////////////////////////
+
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
---
+++ form.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const pdfMake = require('./pdfmake/pdfmake.min')
+const vfsFonts = require('./pdfmake/vfs_fonts');
+
+// pdfMake.vfs = vfsFonts.pdfMake.vfs;
+pdfMake.vfs = vfsFonts.pdfMake.vfs
+
+router.post('/pdf', (req, res, next)=>{
+    // res.send('PDF');
+
+    const name = req.body.name;
+    const email = req.body.email;
+
+    // res.send(`Your name is: ${name}`)
+
+    var docDefinition = {
+        content: [
+            `Hello ${name}` ,
+            `Your email address is ${email}`,
+            'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+        ]
+    };
+
+    // pdfMake.createPdf(docDefinition).download();
+    const pdfDoc = pdfMake.createPdf(docDefinition);
+    pdfDoc.getBase64((data)=>{
+        res.writeHead(200, 
+        {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition':'attachment;filename="filename.pdf"'
+        });
+
+        const download = Buffer.from(data.toString('utf-8'), 'base64');
+        res.end(download);
+    });
+});
+
+module.exports = router;
